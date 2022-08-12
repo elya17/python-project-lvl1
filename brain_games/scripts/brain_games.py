4c21fa82b7fcdab5babe2ce7fecf1053cf0dc755
elya <elechka.mordikova@gmail.com>
2022-08-12 17:08:36 +0300

    third step is completed but with mistake

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-from brain_games.cli import welcome_user
 def welcome():
     print('Welcome to the Brain Games!')
 def main():
     welcome()
-welcome_user()
 if __name__ == '__main__':
     main()
-    
+
+from brain_games.cli import welcome_user
+welcome_user()
