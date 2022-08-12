0f03c85c832558744c04f6a6ae0590a32a787f2b
elya <elechka.mordikova@gmail.com>
2022-08-12 16:47:46 +0300

    created cli.py

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
+from cli import welcome_user
 def welcome():
     print('Welcome to the Brain Games!')
 def main():
     welcome()
+welcome_user()
 if __name__ == '__main__':
     main()
+    
