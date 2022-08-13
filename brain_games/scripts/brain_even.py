f74aef7ba44b095203a72fb1f697194f571344dc
elya <elechka.mordikova@gmail.com>
2022-08-13 17:10:26 +0300

    fifth step also completed(exept askinema)

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -4,25 +4,28 @@ print('Answer "yes" if the number is even, otherwise answer "no"')
 
 
 def game():
-	num = random.randint(1, 1000)
-	if num % 2 == 0:
-		parity = 'yes'
-	else:
-		parity = 'no'
-	print('Question:', num)
-	flag = False
-	while not flag:
-		answer = input('Your answer: ')
-		if answer != parity:
-			print(f"'{answer}' is wrong answer ;(. Correct answer was '{parity}'")
-			print(f"Let's try again, {name!}")
-		else:
-			print('Correct!')
-			flag = True
+    count = 0
+    while count < 3:
+        num = random.randint(1, 1000)
+        if num % 2 == 0:
+            parity = 'yes'
+        else:
+            parity = 'no'
+        print('Question:', num)
+        answer = input('Your answer: ')
+        if answer != parity:
+            print(f"'{answer}' is wrong answer ;(. "
+                  f"Correct answer was '{parity}'")
+            print(f"Let's try again, {name}!")
+        else:
+            print('Correct!')
+            count += 1
+    print(f'Congratulations, {name}!')
 
 
-count = 0
-while count < 3:
-	result = game()
-print(f'Congratulations, {name}!')
+def main():
+    game()
 
+
+if __name__ == '__main__':
+    main()
